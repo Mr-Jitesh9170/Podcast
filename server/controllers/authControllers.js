df82a2272fbd54590a9e6badc105e8269c878b61
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2025-01-04 22:13:52 +0530

    changes in cookie and used morgan

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -45,6 +45,7 @@ exports.signInControllers = async (req, res, next) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: 'None'
         });
         res.status(200).json({ message: "User sign in successfully!", userId: user._id })
     } catch (error) {
